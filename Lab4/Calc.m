33017fec0b56ffedcf401716899d41280437adfa
Maurax <marcus3111@gmail.com>
2016-10-01 15:09:06 +0200

    Lab 4 done
    I think

--- Lab4/Calc.m
+++ Lab4/Calc.m
@@ -1,27 +1,40 @@
-%%
-fileID = fopen('wikipedia.txt');
+fileID = fopen('tiny.txt');
 txt = textscan(fileID, '%f');
 M = cell2mat(txt);
 n = M(1);
 
 rand = ones(n)/n;
 
-prob = zeros(n);
+H=zeros(n);
+D=zeros(n);
 for i=2:2:length(M)
-    prob(M(i)+1, M(i+1)+1)=prob(M(i)+1, M(i+1)+1)+1;
+    H(M(i)+1, M(i+1)+1)=H(M(i)+1, M(i+1)+1)+1;
 end
 for i=1:n
-    s = sum(prob(i,:));
+    s = sum(H(i,:));
     for j=1:n
         if s==0
-            prob(i, j)=1/n;
+            D(i, j) = 1/n;
+            %H(i, j)=1/n;
         else
-            prob(i, j)=prob(i, j)/s;
+            H(i, j)=H(i, j)/s;
         end
     end
 end
 
-totProb = rand*0.15+prob*0.85;
+%totProb = rand*0.15+H*0.85;
 start = [1 zeros(1,n-1)];
-start*totProb^100
+%start*totProb^100
+for i=1:100
+    starto = start*(0.85*H+0.85*D+0.15*rand);
+    if(round(starto, 2)==round(start, 2))
+        i-1
+        break;
+    end
+    start=starto;
+end
+start;
+st = [linspace(0, n-1,n);start];
+%sortrows(st', 2)
+P=(0.85*H+0.85*D+0.15*rand);
 fclose(fileID);
--- Lab4/src/PageRank.java
+++ Lab4/src/PageRank.java
@@ -7,13 +7,14 @@ public class PageRank {
 
 	private LinkedList<Integer>[] graph;
 	private Random r = new Random();
+	private int exited = 0;
 
 	public static void main(String[] args) {
 		int rounds = 1000000;
-		String[] files = new String[] { "three", "tiny", "wikipedia", "medium", /* "p2p-Gnutella08-mod" */ };
+		String[] files = new String[] { "three", "tiny", "wikipedia", "medium", "p2p-Gnutella08-mod" };
 		PageRank p = new PageRank();
-		p.run("wikipedia", rounds);
-		p.printGraph();
+		p.run("p2p-Gnutella08-mod", rounds);
+		// p.printGraph();
 		// for (String file : files)
 		// p.run(file, rounds);
 	}
@@ -22,7 +23,7 @@ public class PageRank {
 		readfile(file + ".txt");
 		int[] visited = session(rounds);
 		System.out.println(file + ": ");
-		printResults(visited, rounds);
+		// printResults(visited, rounds);
 		System.out.println();
 	}
 
@@ -31,16 +32,42 @@ public class PageRank {
 		int[] res = new int[n];
 		int current = 0;
 		res[0]++;
+		int[] oldperc = new int[n];
 		for (int i = 0; i < rounds; i++) {
 			if (r.nextInt(100) < 15 || graph[current].isEmpty())
 				current = r.nextInt(n);// jump to random node
 			else
 				current = graph[current].get(r.nextInt(graph[current].size()));
 			res[current]++;
+			int[] perc = div(res, i);
+			if (eq(perc, oldperc)) {
+				System.out.println("Rounds: " + i);
+				exited = i;
+				break;
+			}
+			oldperc = perc;
 		}
 
 		return res;
 
+	}
+
+	private boolean eq(int[] one, int[] two) {
+		for (int i = 0; i < one.length; i++)
+			if (one[i] != two[i])
+				return false;
+		return true;
+	}
+
+	private int[] div(int[] vec, int num) {
+		int[] res = new int[vec.length];
+		for (int i = 0; i < vec.length; i++)
+			res[i] = (int) Math.round(100 * ((double) vec[i] / num));
+		return res;
+	}
+
+	public int getE() {
+		return exited;
 	}
 
 	public void readfile(String file) {
@@ -65,7 +92,7 @@ public class PageRank {
 	public void printResults(int[] res, int rounds) {
 		System.out.println("Node\tVisited\t\tPercent");
 		for (int i = 0; i < res.length; i++)
-			System.out.println(i + "\t" + res[i] + "\t\t" + res[i] * 100.0 / rounds);
+			System.out.println(i + "\t" + res[i] + "\t\t" + res[i] * 100.0 / exited);
 	}
 
 	public void printGraph() {
