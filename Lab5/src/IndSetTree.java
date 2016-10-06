71ee38574f444c03c8655f3cfbbc92c79297a9b8
Maurax <marcus3111@gmail.com>
2016-10-06 14:29:19 +0200

    one step closer

--- Lab5/src/IndSetTree.java
+++ Lab5/src/IndSetTree.java
@@ -9,11 +9,87 @@ public class IndSetTree {
 	LinkedList<Integer>[] G;
 	LinkedList<Integer>[] T;
 	LinkedList<Integer>[] TNodes;
+	int width;
+	int[][] solution;
 
 	public static void main(String[] args) {
 		IndSetTree i = new IndSetTree();
 		i.readInput(".\\Graphs\\HouseGraph");
 		i.printNodes();
+	}
+
+	public int alg() {
+		for (int i = 0; i < T.length; i++)
+			comp(i);
+		return 0;
+	}
+
+	private int comp(int node) {
+		if (T[node].isEmpty()) { // if leaf
+
+		} else {
+
+		}
+		return 0;
+	}
+
+	public int R0(LinkedList<Integer>[] graph) {
+		// case 1:
+		if (isEmpty(graph))
+			return 0;
+		// case 2:
+		for (int i = 0; i < graph.length; i++)
+			if (graph[i] != null && graph[i].size() == 0) {
+				graph[i] = null;
+				return 1 + R0(graph);
+			}
+
+		// case 3:
+		int max = 0;
+		int maxN = -1;
+
+		for (int i = 0; i < graph.length; i++)// get max neighbors
+			if (graph[i] != null && graph[i].size() > max) {
+				max = graph[i].size();
+				maxN = i;
+			}
+		LinkedList<Integer>[] graph2 = copy(graph);
+		remove(graph, maxN);
+		while (!graph2[maxN].isEmpty())
+			if (graph2[graph2[maxN].getFirst()] != null)
+				remove(graph2, graph2[maxN].getFirst());
+		remove(graph2, maxN);
+		return Math.max(R0(graph), 1 + R0(graph2));
+	}
+
+	private boolean isEmpty(LinkedList<Integer>[] graph) {
+		for (LinkedList l : graph)
+			if (l != null)
+				return false;
+		return true;
+	}
+
+	private LinkedList<Integer>[] copy(LinkedList<Integer>[] graph) {
+		LinkedList<Integer>[] cpy = new LinkedList[graph.length];
+		int i = 0;
+		for (LinkedList<Integer> l : graph) {
+			LinkedList<Integer> c;
+			if (l == null)
+				c = null;
+			else {
+				c = new LinkedList<Integer>();
+				for (int j : l)
+					c.add(j);
+			}
+			cpy[i++] = c;
+		}
+		return cpy;
+	}
+
+	private void remove(LinkedList<Integer>[] graph, Integer node) {
+		for (int i : graph[node])
+			graph[i].remove(node);
+		graph[node] = null;
 	}
 
 	public void readInput(String file) {
@@ -54,6 +130,8 @@ public class IndSetTree {
 				head = line.split(" ");
 			}
 			int n = Integer.parseInt(head[2]);
+			width = Integer.parseInt(head[3]);
+			solution = new int[n][(int) Math.pow(2, width)];
 
 			T = new LinkedList[n];
 			TNodes = new LinkedList[n];
@@ -76,7 +154,6 @@ public class IndSetTree {
 				int node1 = Integer.parseInt(edge[0]) - 1;
 				int node2 = Integer.parseInt(edge[1]) - 1;
 				T[node1].add(node2);
-				T[node2].add(node1);
 				line = br.readLine();
 			}
 		} catch (FileNotFoundException e) {
---
+++ Lab5/src/Node.java
@@ -0,0 +1,79 @@
+import java.util.LinkedList;
+
+public class Node {
+	LinkedList<Integer> nodes;
+	LinkedList<Integer>[] graph;
+	int parent;
+	LinkedList<Node> children;
+	int[] indSets;
+	String[] permutations;
+
+	public static void main(String[] args) {
+
+	}
+
+	public Node(LinkedList<Integer> nodes, LinkedList<Integer>[] graph, int parent, LinkedList<Node> children) {
+		this.nodes = nodes;
+		this.graph = graph;
+		this.parent = parent;
+		this.children = children;
+		indSets = new int[(int) Math.pow(2, graph.length)];
+
+		permutations = new String[(int) Math.pow(2, graph.length)];
+		for (int i = 0; i < Math.pow(2, graph.length); i++) {
+			permutations[i] = "";
+			String bin = Integer.toBinaryString(i);
+			for (int j = 0; j < graph.length - bin.length(); j++)
+				permutations[i] += "0";
+			permutations[i] += bin;
+		}
+	}
+
+	public void solveNode() {
+		if (children.isEmpty())
+			for (int i = 0; i < permutations.length; i++)
+				indSets[i] = addSol(permutations[i]);
+		else
+			for (int i = 0; i < permutations.length; i++) {
+				int max = 0;
+				for (Node n : children)
+					max += n.getPermMax(permutations[i]);
+				indSets[i] = addSol(permutations[i]) + max;
+			}
+	}
+
+	private int addSol(String perm) {
+		LinkedList<Integer> marked = new LinkedList<Integer>();
+		for (int i = 0; i < perm.length(); i++)
+			if (perm.charAt(i) == '1')
+				marked.add(i);
+		for (int n : marked)
+			for (int m : marked)
+				if (graph[n].contains(m))
+					return -1;
+		return marked.size();
+	}
+
+	public int getNodeIndex(int node) {
+		return nodes.indexOf(node);
+	}
+
+	public int getSize() {
+		return nodes.size();
+	}
+
+	public int getPermMax(String perm) {
+		String p = "";
+		for (int i = 0; i < p.length(); i++)
+			if (perm.charAt(i) == '1')
+				p += '1';
+			else
+				p += '.';
+
+		int max = -1;
+		for (int i = 0; i < permutations.length; i++)
+			if (permutations[i].matches(p) && indSets[i] > max)
+				max = indSets[i];
+		return max;
+	}
+}
